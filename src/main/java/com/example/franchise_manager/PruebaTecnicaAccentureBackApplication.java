2b19a1cda8ccb9691b59475db09b7f6191f585d2
Alejandro Muñoz Acevedo <mello0616@gmail.com>
2026-04-13 20:02:56 -0500

    first commit

---
+++ src/main/java/com/example/franchise_manager/PruebaTecnicaAccentureBackApplication.java
@@ -0,0 +1,14 @@
+package com.example.franchise_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaAccentureBackApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando aplicación...");
+		SpringApplication.run(PruebaTecnicaAccentureBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/franchise_manager/PruebaTecnicaAccentureBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.franchise_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaAccentureBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
